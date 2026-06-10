54bbbd0ba63aad59b837a492cc027ebd8695cf13
@BekhaDavlatov <davlatovbekhruz30@gmail.com>
2026-06-10 12:21:33 +0200

    works on the props

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import TodoItem from "./TodoItem";
+const TodoList = (props) => {
+    const {
+        tasks = [],
+    } = props
+
+    const hasTasks = true;
+    if (!hasTasks) {
+        return <div className="todo__empty-message"></div>
+    } return (
+        <ul className="todo__list">
+            {tasks.map((tasks) => (
+                <TodoItem
+                    className="todo__item"
+                    id={tasks.id}
+                    title={tasks.title}
+                    isDone={tasks.isDone}
+                />
+            ))}
+        </ul>
+    )
+
+}
+
+export default TodoList;
